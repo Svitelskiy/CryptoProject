a0ff7c564b41e53c90623441aedd1d2a60395d44
Svitelskiy <85756206+Svitelskiy@users.noreply.github.com>
2022-01-18 18:24:13 +0200

    added Chrome

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture(scope="function")
+def browser():
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.get("https://coinmarketcap.com/")
+    driver.delete_all_cookies()
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="function")
+def coin():
+    return {"coin_ticker": input("text coin ticker: ")}
